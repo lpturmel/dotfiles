52ed9f880836a12e01574ffa2b042594af6dc853
Louis-Philippe Turmel <turmlo@2020spaces.com>
2021-12-13 23:27:54 -0500

    Updated setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Setup zsh common theme
+echo "Installing common zsh theme..."
 wget -O $ZSH_CUSTOM/themes/common.zsh-theme https://raw.githubusercontent.com/jackharrisonsherlock/common/master/common.zsh-theme
 echo "Done installing common zsh theme..."
-echo "Run :CocInstall coc-prettier in vim to finish the prettier installation!"
