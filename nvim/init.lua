c9de212faae79612c9dc98f02847a7ba767d1a1a
Louis-Philippe Turmel <turmlo@2020spaces.com>
2021-12-22 20:13:38 -0500

    Modified init.lua to use vim.o wrapper

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,47 +1,41 @@
 vim.o.encoding = 'utf-8'
--- Option not available in Neovim ???
--- vim.api.nvim_set_option('nocompatible', true)
 vim.o.number = true
 vim.o.relativenumber = true
 vim.o.title = true
 vim.o.autoindent = true
 vim.o.backup = false
 vim.o.hlsearch = true
-vim.api.nvim_set_option('hlsearch', true)
-vim.api.nvim_set_option('showcmd', true)
-vim.api.nvim_set_option('cmdheight', 1)
-vim.api.nvim_set_option('laststatus', 2)
-vim.api.nvim_set_option('scrolloff', 10)
-vim.api.nvim_set_option('sidescrolloff', 10)
-vim.api.nvim_set_option('expandtab', true)
-vim.api.nvim_set_option('backupskip', '/tmp/*,/private/tmp/*')
+vim.o.showcmd = true
+vim.o.cmdheight = 1
+vim.o.laststatus = 2
+vim.o.scrolloff = 10
+vim.o.sidescrolloff = 10
+vim.o.expandtab = true
+vim.o.backupskip = '/tmp/*,/private/tmp/*'
 -- Neovim specific
-vim.api.nvim_set_option('inccommand', 'split')
---
--- Not available in neomvim
--- vim.api.nvim_set_option('t_BE', nil)
-vim.api.nvim_set_option('scb', false)
-vim.api.nvim_set_option('ru', false)
-vim.api.nvim_set_option('showmatch', false)
-vim.api.nvim_set_option('lazyredraw', true)
-vim.api.nvim_set_option('ignorecase', true)
-vim.api.nvim_set_option('smarttab', true)
-vim.api.nvim_set_option('shiftwidth', 4)
-vim.api.nvim_set_option('tabstop', 4)
-vim.api.nvim_set_option('ai', true)
-vim.api.nvim_set_option('si', true)
-vim.api.nvim_set_option('wrap', false)
-vim.api.nvim_set_option('cursorline', true)
-vim.api.nvim_set_option('suffixesadd', '.js,.es,.jsx,.json,.css,.less,.sass,.styl,.php,.py,.md')
-vim.api.nvim_set_option('exrc', true)
-vim.api.nvim_set_option('hidden', true)
+vim.o.inccommand = 'split'
+vim.o.scrollbind = false
+vim.o.ruler = false
+vim.o.showmatch = false
+vim.o.lazyredraw = true
+vim.o.ignorecase = true
+vim.o.smarttab = true
+vim.o.shiftwidth = 4
+vim.o.tabstop = 4
+vim.o.autoindent = true
+vim.o.smartindent = true
+vim.o.wrap = false
+vim.o.cursorline = true
+vim.o.suffixesadd = '.c,.cpp,.h,.hpp,.hxx,.m,.mm,.php,.py,.rb,.sh,.swift,.yml,.yaml,.rust,.json,.js,.jsx,.ts,.tsx,.css,.md,.es'
+vim.o.exrc = true
+vim.o.hidden = true
 
 -- Colors
-vim.api.nvim_set_option('background', "dark")
-vim.api.nvim_set_option('termguicolors', true)
-vim.api.nvim_set_option('winblend', 0)
-vim.api.nvim_set_option('wildoptions','pum')
-vim.api.nvim_set_option('pumblend', 5)
+vim.o.background = 'dark'
+vim.o.termguicolors = true
+vim.o.winblend = 0
+vim.o.wildoptions = 'pum'
+vim.o.pumblend = 5
 vim.cmd('colorscheme onedark')
 
 -- Globals
