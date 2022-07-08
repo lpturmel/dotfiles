604a4866ab70a8186b79b4849a0d340ffa15b9a5
Louis-Philippe Turmel <turmlo@2020spaces.com>
2022-07-08 10:10:17 -0400

    Added zsh plugins to install script

--- setup.sh
+++ setup.sh
@@ -6,6 +6,9 @@ echo "Setting zshrc..."
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/nvim ~/.config/nvim
 
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
 echo "Installing Neovim package manager (packer)"
 git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 echo "Done!"
