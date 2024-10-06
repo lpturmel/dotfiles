46c6b42152a28c6a0aa1768455e083318de148eb
Louis-Philippe Turmel <louisphilippe.turmel@gmail.com>
2024-10-06 14:48:07 -0400

    Updated packages and added autocmd for lsps

--- nvim/lua/config/lsp/init.lua
+++ nvim/lua/config/lsp/init.lua
@@ -23,63 +23,63 @@ configs.hulkls = {
 local function config(_config)
     return vim.tbl_deep_extend("force", {
         capabilities = default_capabilities,
-        on_attach = function()
-            local opts = { noremap = true, silent = true }
-
-            vim.keymap.set("n", "gD", function()
-                vim.lsp.buf.declaration()
-            end, opts)
-
-            vim.keymap.set("n", "gd", function()
-                vim.lsp.buf.definition()
-            end, opts)
-
-            vim.keymap.set("n", "K", function()
-                vim.lsp.buf.hover()
-            end, opts)
-
-            vim.keymap.set("n", "gi", function()
-                vim.lsp.buf.implementation()
-            end, opts)
-
-            vim.keymap.set("n", "<C-k>", function()
-                vim.lsp.buf.signature_help()
-            end, opts)
-
-            vim.keymap.set("n", "gr", function()
-                vim.lsp.buf.references()
-            end, opts)
-
-            vim.keymap.set('n', '<space>rn', function()
-                vim.lsp.buf.rename()
-            end, opts)
-
-            vim.keymap.set('n', '<space>ca', function()
-                vim.lsp.buf.code_action()
-            end, opts)
-
-            vim.keymap.set('n', '<space>h', function()
-                vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
-            end, opts)
-
-            vim.keymap.set('n', '[d', function()
-                vim.diagnostic.goto_prev({ border = "rounded" })
-            end, opts)
-
-            vim.keymap.set(
-                "n",
-                "gl",
-                function()
-                    vim.lsp.diagnostic.show_line_diagnostics({ border = "rounded" })
-                end, opts)
-            vim.keymap.set("n", "<C-s>", function()
-                vim.diagnostic.goto_next({ border = "rounded" })
-            end, opts)
-
-            vim.keymap.set("n", "<leader>q", function()
-                vim.diagnostic.setloclist()
-            end, opts)
-        end
+        -- on_attach = function()
+            -- local opts = { noremap = true, silent = true }
+            --
+            -- vim.keymap.set("n", "gD", function()
+            --     vim.lsp.buf.declaration()
+            -- end, opts)
+            --
+            -- vim.keymap.set("n", "gd", function()
+            --     vim.lsp.buf.definition()
+            -- end, opts)
+            --
+            -- vim.keymap.set("n", "K", function()
+            --     vim.lsp.buf.hover()
+            -- end, opts)
+            --
+            -- vim.keymap.set("n", "gi", function()
+            --     vim.lsp.buf.implementation()
+            -- end, opts)
+            --
+            -- vim.keymap.set("n", "<C-k>", function()
+            --     vim.lsp.buf.signature_help()
+            -- end, opts)
+            --
+            -- vim.keymap.set("n", "gr", function()
+            --     vim.lsp.buf.references()
+            -- end, opts)
+            --
+            -- vim.keymap.set('n', '<space>rn', function()
+            --     vim.lsp.buf.rename()
+            -- end, opts)
+            --
+            -- vim.keymap.set('n', '<space>ca', function()
+            --     vim.lsp.buf.code_action()
+            -- end, opts)
+            --
+            -- vim.keymap.set('n', '<space>h', function()
+            --     vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
+            -- end, opts)
+            --
+            -- vim.keymap.set('n', '[d', function()
+            --     vim.diagnostic.goto_prev({ border = "rounded" })
+            -- end, opts)
+            --
+            -- vim.keymap.set(
+            --     "n",
+            --     "gl",
+            --     function()
+            --         vim.lsp.diagnostic.show_line_diagnostics({ border = "rounded" })
+            --     end, opts)
+            -- vim.keymap.set("n", "<C-s>", function()
+            --     vim.diagnostic.goto_next({ border = "rounded" })
+            -- end, opts)
+            --
+            -- vim.keymap.set("n", "<leader>q", function()
+            --     vim.diagnostic.setloclist()
+            -- end, opts)
+        -- end
     }, _config or {})
 end
 
@@ -87,7 +87,7 @@ lsp_installer.setup({
     ensure_installed = {
         "jsonls",
         "rust_analyzer",
-        "tsserver",
+        "ts_ls",
         "astro",
         "tailwindcss",
         "bashls",
@@ -97,7 +97,7 @@ lsp_installer.setup({
 })
 
 lspconfig.gopls.setup(config {})
-lspconfig.tsserver.setup(config(require "config.lsp.settings.tsserver"))
+lspconfig.ts_ls.setup(config(require "config.lsp.settings.ts_ls"))
 -- lspconfig.lua_ls.setup(config({
 --     settings = {
 --         Lua = {
@@ -172,6 +172,69 @@ vim.api.nvim_create_autocmd("BufWritePre", {
                 end
             end
         })
+    end
+})
+
+vim.api.nvim_create_autocmd("LspAttach", {
+    callback = function(args)
+        -- local bufnr = args.buf
+        local client = assert(vim.lsp.get_client_by_id(args.data.client_id), "must have valid client")
+
+        local opts = { noremap = true, silent = true }
+
+        vim.keymap.set("n", "gD", function()
+            vim.lsp.buf.declaration()
+        end, { buffer = 0 })
+
+        vim.keymap.set("n", "gd", function()
+            vim.lsp.buf.definition()
+        end, { buffer = 0 })
+
+        vim.keymap.set("n", "K", function()
+            vim.lsp.buf.hover()
+        end, { buffer = 0 })
+
+        vim.keymap.set("n", "gi", function()
+            vim.lsp.buf.implementation()
+        end, { buffer = 0 })
+
+        vim.keymap.set("n", "<C-k>", function()
+            vim.lsp.buf.signature_help()
+        end, { buffer = 0 })
+
+        vim.keymap.set("n", "gr", function()
+            vim.lsp.buf.references()
+        end, { buffer = 0 })
+
+        vim.keymap.set('n', '<space>rn', function()
+            vim.lsp.buf.rename()
+        end, { buffer = 0 })
+
+        vim.keymap.set('n', '<space>ca', function()
+            vim.lsp.buf.code_action()
+        end, { buffer = 0 })
+
+        vim.keymap.set('n', '<space>h', function()
+            vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
+        end, { buffer = 0 })
+
+        vim.keymap.set('n', '[d', function()
+            vim.diagnostic.goto_prev({ border = "rounded" })
+        end, { buffer = 0 })
+
+        vim.keymap.set(
+            "n",
+            "gl",
+            function()
+                vim.lsp.diagnostic.show_line_diagnostics({ border = "rounded" })
+            end, { buffer = 0 })
+        vim.keymap.set("n", "<C-s>", function()
+            vim.diagnostic.goto_next({ border = "rounded" })
+        end, { buffer = 0 })
+
+        vim.keymap.set("n", "<leader>q", function()
+            vim.diagnostic.setloclist()
+        end, { buffer = 0 })
     end
 })
 
